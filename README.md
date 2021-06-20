c7c50a2315d2436d8937058815d72b6f927f9b3a
Rakib Raza <rakibraza159@gmail.com>
2021-06-20 23:27:51 +0600

    add user API done

---
+++ README.md
@@ -0,0 +1,56 @@
+## Website Name
+
+### `FastPro - Cleaning Services`
+
+## Website Link
+
+(FastPro - Cleaning Services)[https://fastpro-bd.web.app/]
+
+## Website Feature
+
+#### `Firebase Authentication`
+
+#### `SignIn Method`
+
+1. Google
+
+#### `Backend Tecnology`
+
+1. NodeJS
+2. ExpressJS
+3. MongoDB
+4. Heroku
+
+#### `Front-End Tecnology`
+
+1. HTML
+2. CSS
+3. JavaScript
+4. ReactJS
+5. Firebase
+
+#### `Home Page`
+
+1. Header
+2. Services
+3. About Us
+4. Team Meambers
+5. Contact Form
+6. Reviews
+7. Call To Action
+8. Footer
+
+#### `Admin Dashboard`
+
+1. Dashboard
+2. Order List
+3. Add Service
+4. Make Admin
+5. Manage Service
+
+#### `Client Dashaboard`
+
+1. Dashboard
+2. Booking
+3. Booking List
+4. Add Review
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const app = express();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xaixv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const jobCollection = client.db(process.env.DB_NAME).collection("jobs");
+  const userCollection = client.db(process.env.DB_NAME).collection("users");
+
+  // Add Job
+  app.post("/addJob", (req, res) => {
+    const job = req.body;
+    jobCollection
+      .insertOne(job)
+      .then((result) => res.send(result.insertedCount > 0));
+  });
+
+  // Get All Job
+  app.get("/jobs", (req, res) => {
+    jobCollection.find({ status: 'done' }).toArray((err, collection) => res.send(collection));
+  });
+
+  // Add Users
+  app.post("/addUser", (req, res) => {
+    const user = req.body;
+    userCollection.insertOne(user).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App listening at Port:${port}`);
+});
